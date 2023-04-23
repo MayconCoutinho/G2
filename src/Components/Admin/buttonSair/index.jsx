0f6c7de1da2de43c35093dc7650ee6985522cb14
MayconCoutinho <mayconcoutinhoart@gmail.com>
2023-04-23 15:51:06 -0300

    atualizando nome das pastas

--- src/Components/Admin/buttonSair/index.jsx
+++ src/Components/Admin/buttonSair/index.jsx
@@ -1,28 +1,35 @@
-import { Button } from "@mui/material"
-import { useNavigate } from "react-router-dom"
-import { goToLogin } from "../../../routes/coordinator"
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { goToLogin } from "../../../routes/coordinator";
 
 export const ButtonSair = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const Sair = () => {
-    localStorage.removeItem('tokenPassword')
-    localStorage.removeItem('tokenName')
-    goToLogin(navigate)
-  }
-  return <>
-    <Button
-      variant="contained"
-      sx={{
-        background: "#5a5a5a",
-        borderRadius:1,
-        marginTop:.8,
-        marginBottom:.8,
-        '&:hover': {
-            backgroundColor: '#b4b4b44b',
-        },
-        fontSize: "14px",
-        color: "#fff",
-    }}
-      onClick={() => { Sair() }}> Sair </Button>
-  </>
-}
+    localStorage.removeItem("tokenPassword");
+    localStorage.removeItem("tokenName");
+    goToLogin(navigate);
+  };
+  return (
+    <>
+      <Button
+        variant="contained"
+        sx={{
+          background: "#5a5a5a",
+          borderRadius: 1,
+          marginTop: 0.8,
+          marginBottom: 0.8,
+          "&:hover": {
+            backgroundColor: "#b4b4b44b",
+          },
+          fontSize: "14px",
+          color: "#fff",
+        }}
+        onClick={() => {
+          Sair();
+        }}
+      >
+        Sair
+      </Button>
+    </>
+  );
+};
